93b014cae19ee128700d2bf6c60f3c8f565c977b
Ethanscharlie <ethanscharlie@protonmail.com>
2023-10-22 20:25:19 -0600

    updated

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar"
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar example &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+     MONITOR=$m polybar -q top -c "$DIR"/config.ini &
+#     MONITOR=$m polybar -q bottom -c "$DIR"/config.ini &
+  done
+else
+  polybar --reload example &
+fi
